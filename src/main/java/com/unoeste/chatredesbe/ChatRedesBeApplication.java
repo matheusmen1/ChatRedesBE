e3aebb8243bf43aa21e3e7585b8985a840b5d5dc
matheusmen1 <antonuccimatheus1@outlook.com>
2026-05-27 16:00:27 -0300

    feat: Inicializando o projeto de redes.

---
+++ src/main/java/com/unoeste/chatredesbe/ChatRedesBeApplication.java
@@ -0,0 +1,13 @@
+package com.unoeste.chatredesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRedesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRedesBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unoeste/chatredesbe/ChatRedesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unoeste.chatredesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRedesBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
